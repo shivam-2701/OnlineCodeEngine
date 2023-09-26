87f697467e7f3455f39be3f911fab5cf7b96f001
Shivam Kumar <shivam-kumar-shah@outlook.com>
2023-09-26 06:23:59 +0000

    build: :sparkles: add https server

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -12,14 +12,10 @@ const extensions = {
 
 const runCode = async (apiBody, channel, msg) => {
   try {
-  client.set(apiBody.folder.toString(), "Processing");
-  const absolutePath = path.resolve('./temp');
-  const command = `docker run --rm --mount type=bind,source=${absolutePath},target=/app,readonly=false -t compiler:v7a ${extensions[apiBody.lang]
-      } ${apiBody.folder}/source.${extensions[apiBody.lang]} 5`;
     client.set(apiBody.folder.toString(), "Processing");
-    const command = `docker run --rm --mount type=bind,source=./temp,target=/app,readonly=false -t compiler:v7a ${
-      extensions[apiBody.lang]
-    } ${apiBody.folder}/source.${extensions[apiBody.lang]} 5`;
+    const absolutePath = path.resolve('./temp');
+    const command = `docker run --rm --mount type=bind,source=${absolutePath},target=/app,readonly=false -t compiler:v7a ${extensions[apiBody.lang]
+      } ${apiBody.folder}/source.${extensions[apiBody.lang]} 5`;
     await fs.promises.writeFile(`./temp/${apiBody.folder}/output.txt`, "");
     const output: Record<string, any> = await execute(command);
     const data = await fs.promises.readFile(
